ff324d66a3df8c6a5fa5f3991d5470e233211aab
matyaya76 <matyaya76@gmail.com>
2022-09-29 21:02:30 +0900

    first commit

---
+++ repos/example/example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int money;
+            string name;
+
+            money = 5000;
+            name = "北村";
+
+            Console.WriteLine(money);
+            Console.WriteLine(name);
+        }
+    }
+}
